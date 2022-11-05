7482c2a92678ee411d63daad30b65121622695ea
Fandor626 <rarikovi45690@gmail.com>
2022-11-05 23:59:17 +0200

    done first lab

---
+++ app/src/main/java/com/example/lab2navdrawer/ui/myFragment/MyViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.lab2navdrawer.ui.myFragment
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MyViewModel: ViewModel(){
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is My Fragment"
+    }
+    val text: LiveData<String> = _text
+}
